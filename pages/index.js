5f02728c0716d57a14afff4dfacf0fba5ef45894
Lukas Frankl <lukinfx@gmail.com>
2023-06-30 21:39:57 +0200

    Test 1

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,41 @@
+import { useState } from "react";
 import Head from "next/head";
 import Header from "@components/Header";
 
 export default function Home() {
+  const [showText, setShowText] = useState(false);
+
+  const handleClick = () => {
+    setShowText(true);
+  };
+
   return (
     <div className="container">
       <main>
+        <Head>
+          <title>Next.js App</title>
+        </Head>
         <Header title="Next.js Toolbox" />
         <hr />
-        <p className="description" style={{ color: 'green' }}>
-          Koupim ti Mustanga
-        </p>
-        <p className="description" style={{ color: 'blue', fontSize: '6px' }}>
-          Na dalkove ovladani
-        </p>
+        {!showText && (
+          <a href="#" onClick={handleClick}>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/800px-Heart_coraz%C3%B3n.svg.png"
+              alt="Button Image"
+              className="button-image"
+              width={100} // Set the desired width of the image
+              height={100} // Set the desired height of the image
+            />
+          </a>
+        )}
+        {showText && (
+          <p
+            className="description"
+            style={{ color: 'green', fontFamily: 'cursive' }}
+          >
+            Drahá Verunko, chtěl bych ti sdělit, že jsi překrásná a milá a snaživá holka, kterou bezmezně miluji. Vždy při tobě budu stát a vždy ti se vším pomůžu, na život od teď nejsi sama. A teď mi pochval mou aplikaci.
+          </p>
+        )}
       </main>
     </div>
   );
