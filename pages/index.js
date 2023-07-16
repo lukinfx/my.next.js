3d8a184879b51628e18a0ad425cbad0d1677a082
Lukas Frankl <lukinfx@gmail.com>
2023-07-16 12:47:21 +0200

    openAI API

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
 
-function IndexPage({ allPosts }) {
+export default function Home() {
+  const [animalInput, setAnimalInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ animal: animalInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+      debugger;
+      setResult(data.result);
+      setAnimalInput("");
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
   return (
     <div>
-      <h1>Hello, Next.js!</h1>
-      
+      <Head>
+        <title>OpenAI Quickstart</title>
+        <link rel="icon" href="/dog.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/dog.png" className={styles.icon} />
+        <h3>Name my pet</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="animal"
+            placeholder="Enter an animal"
+            value={animalInput}
+            onChange={(e) => setAnimalInput(e.target.value)}
+          />
+          <input type="submit" value="Generate names" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
     </div>
   );
 }
@@ -18,5 +66,3 @@ function IndexPage({ allPosts }) {
 //     props: { allPosts },
 //   };
 // }
-
-export default IndexPage;
